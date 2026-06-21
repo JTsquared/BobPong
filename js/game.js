d75ce4eadd8952d7bb5893be4d29c449ab406025
Jonathan.turner <turner.e.jonathan@gmail.com>
2026-06-21 15:41:53 -0400

    changing websocket url

--- js/game.js
+++ js/game.js
@@ -120,7 +120,7 @@ export class Game {
         this.onlineLobbyState = 'choose'; // 'choose', 'hosting', 'joining', 'connecting', 'connected'
         this.roomCodeInput = '';
         this.onlineStatusMessage = '';
-        this.signalingUrl = 'ws://localhost:3000'; // Override for production
+        this.signalingUrl = 'ws://193.122.187.79:3001';
     }
 
     getCurrentZones() {
